8789ade82b334050ee8d03aae9a3c0c0bb82edc2
hanahatem1 <hanahatemm88@gmail.com>
2025-05-16 00:30:16 +0300

    add profile photo

--- lib/features/profile/views/profile_view.dart
+++ lib/features/profile/views/profile_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/constant/app_colors.dart';
+import 'package:shopping_app/features/profile/widgets/profile_photo.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: AppColors.backGroundColor,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal:12,vertical: 12),
+        child: Column(
+          children: [
+            SizedBox(height: 70,),
+            ProfilePhoto()
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/profile/widgets/profile_photo.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/constant/app_colors.dart';
+
+class ProfilePhoto extends StatelessWidget {
+  const ProfilePhoto({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(children: [
+        const SizedBox(
+          height: 110,
+          width: 110,
+          child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/picture.jpg')),
+        ),
+        Positioned(
+            bottom: 10,
+            right: 3,
+            child: Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.backGroundColor,
+              ),
+              child:const Padding(
+                padding: EdgeInsets.all(4),
+                child: Icon(Icons.add_a_photo_outlined, size: 15),
+              ),
+            ))
+      ]),
+    );
+  }
+}
