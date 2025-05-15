e9a24dcbda4016ff4c066891393fbe7118f8e546
hanahatem1 <hanahatemm88@gmail.com>
2025-05-15 20:58:03 +0300

    card item fir favourites

--- lib/features/favourites/views/favourites_view.dart
+++ lib/features/favourites/views/favourites_view.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/constant/app_colors.dart';
 
 class FavouritesView extends StatelessWidget {
   const FavouritesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.backGroundColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.backGroundColor,
+        centerTitle: true,
+        title:const Text('Favouites',style: TextStyle(fontWeight: FontWeight.bold),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:12,vertical: 12),
+        child: Column(
+          children: [
+            Card(
+              color: AppColors.cardColor,
+              elevation: 2,
+              child:Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                child: Row(
+                  children: [
+                    Image.asset('assets/images/b.jpg',height: 120,width: 120,),
+                    const SizedBox(width: 14,),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Bijama set',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                        SizedBox(height: 10,),
+                        Text('\$49',style: TextStyle(color: AppColors.secondryColor),)
+                      ],
+                    ),
+                    const SizedBox(width: 90,),
+                    Image.asset('assets/images/Component 1.png',height: 30,width: 30,)
+                  ],
+                ),
+              ) ,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
