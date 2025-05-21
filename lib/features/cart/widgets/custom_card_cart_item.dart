82b22770904802fff97a1806f8412a17a797a1ac
hanahatem1 <hanahatemm88@gmail.com>
2025-05-21 15:45:46 +0300

    add dismissible widget

--- lib/features/cart/widgets/custom_card_cart_item.dart
+++ lib/features/cart/widgets/custom_card_cart_item.dart
@@ -4,49 +4,61 @@ import 'package:shopping_app/features/cart/widgets/custom_container_count.dart';
 import 'package:shopping_app/features/home/data/models/product_model.dart';
 
 class CustomCardCartItem extends StatelessWidget {
-  const CustomCardCartItem({super.key, required this.product});
+   CustomCardCartItem({super.key, required this.product,required this.onDismissed});
 final ProductModel product;
+void Function(DismissDirection)? onDismissed;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.cardColor,
-      elevation: 2,
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Image.network(
-              product.image,
-              height: 120,
-              width: 120,
+    return Dismissible(
+      key: Key(product.id.toString()),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: AppColors.backGroundColor,
+        alignment: Alignment.centerRight,
+        padding:const EdgeInsets.symmetric(horizontal: 16),
+        child: const Icon(Icons.delete,color: AppColors.primaryColor,),
+      ),
+      onDismissed: onDismissed,
+      child: Card(
+        color: AppColors.cardColor,
+        elevation: 2,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Image.network(
+                product.image,
+                height: 120,
+                width: 120,
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 14,
-          ),
-           Expanded(
-             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                product.title,
-                  style:const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  '\$${product.price}',
-                  style:const TextStyle(color: AppColors.secondryColor),
-                )
-              ],
-                       ),
-           ),
-          const SizedBox(
-            width: 30,
-          ),
-          const CustomContainerCount()
-        ],
+            const SizedBox(
+              width: 14,
+            ),
+             Expanded(
+               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                  product.title,
+                    style:const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '\$${product.price}',
+                    style:const TextStyle(color: AppColors.secondryColor),
+                  )
+                ],
+                         ),
+             ),
+            const SizedBox(
+              width: 30,
+            ),
+            const CustomContainerCount()
+          ],
+        ),
       ),
     );
   }
--- lib/features/cart/widgets/custom_listview_cart.dart
+++ lib/features/cart/widgets/custom_listview_cart.dart
@@ -4,9 +4,14 @@ import 'package:shopping_app/features/cart/data/cart_cubit/cart_cubit.dart';
 import 'package:shopping_app/features/cart/data/cart_cubit/cart_state.dart';
 import 'package:shopping_app/features/cart/widgets/custom_card_cart_item.dart';
 
-class CustomListviewCart extends StatelessWidget {
+class CustomListviewCart extends StatefulWidget {
   const CustomListviewCart({super.key});
 
+  @override
+  State<CustomListviewCart> createState() => _CustomListviewCartState();
+}
+
+class _CustomListviewCartState extends State<CustomListviewCart> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CartCubit, CartState>(
@@ -24,9 +29,17 @@ class CustomListviewCart extends StatelessWidget {
         return ListView.builder(
           itemCount: items.length,
           itemBuilder: (context, index) {
+            final product = items[index];
             return  Padding(
               padding:const  EdgeInsets.only(bottom: 5),
-              child: CustomCardCartItem(product:items[index],),
+              child: CustomCardCartItem(
+                onDismissed: (direction) {
+                  setState(() {
+                   context.read<CartCubit>().removeFromCart(product);
+                  });
+                },
+                product:items[index],)
+                ,
             );
           },
         );
