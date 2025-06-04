2761aaac6b084195fc77f7cd2075623796cd3aff
hanahatem1 <hanahatemm88@gmail.com>
2025-06-04 23:21:31 +0300

    fuctionlity of categories card

--- lib/features/home/presentation/widgets/custom_card_categories.dart
+++ lib/features/home/presentation/widgets/custom_card_categories.dart
@@ -2,20 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:shopping_app/core/constant/app_colors.dart';
 
 class CustomCardCategories extends StatelessWidget {
-  const CustomCardCategories({super.key, required this.title});
+  const CustomCardCategories({super.key, required this.title, required this.isSelected,required this.onTap});
 
 final String title;
+final bool isSelected;
+final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      color: AppColors.cardColor,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-          side:const BorderSide(color: AppColors.primaryColor)),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 5),
-        child: Text(title),
+    return GestureDetector(
+      onTap:onTap ,
+      child: Card(
+        elevation: 1,
+        color: isSelected ? AppColors.primaryColor : AppColors.cardColor,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+            side:const BorderSide(color: AppColors.primaryColor)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 5),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: isSelected ? AppColors.cardColor : AppColors.primaryColor
+            ),
+            ),
+        ),
       ),
     );
   }
--- lib/features/home/presentation/widgets/list_view_categories.dart
+++ lib/features/home/presentation/widgets/list_view_categories.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/features/home/presentation/widgets/custom_card_categories.dart';
 
-class ListViewCategories extends StatelessWidget {
+class ListViewCategories extends StatefulWidget {
   const ListViewCategories({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<String> categories = [
+  State<ListViewCategories> createState() => _ListViewCategoriesState();
+}
+
+class _ListViewCategoriesState extends State<ListViewCategories> {
+   final List<String> categories = [
       'All',
       'Electronic',
       "Men's clothes",
       "Women's clothes",
       'Jewelry',
     ];
+     int selectedIndex=0;
+    @override
+  Widget build(BuildContext context) {
+
     return SizedBox(
       height: 40,
       child: ListView.builder(
@@ -21,7 +28,15 @@ class ListViewCategories extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.only(right: 2),
-            child: CustomCardCategories(title: categories[index]),
+            child: CustomCardCategories(
+              title: categories[index],
+               isSelected: index==selectedIndex,
+              onTap: (){
+                setState(() {
+                  selectedIndex=index;
+                });
+              },
+              ),
           );
         },
       ),
